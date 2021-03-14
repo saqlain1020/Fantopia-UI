d356c3756a0601edc415913d557fdaf0ad908772
Saqlain <saqlainprinters@gmail.com>
2021-03-14 12:54:27 +0500

    Navbar badge

--- src/Components/Navbar/Components/Profile.js
+++ src/Components/Navbar/Components/Profile.js
@@ -1,4 +1,4 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { Badge, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 import { withRouter } from "react-router";
 import HexPng from "src/Assets/Images/hex.png";
@@ -30,50 +30,60 @@ const Profile = (props) => {
       onClick={() => props.history.push("/Account")}
     >
       <Typography className={classes.text}>20 KELEK</Typography>
-      <div style={{ position: "relative", width: 45, height: 50 }}>
-        <img
-          alt=""
-          src={HexPng}
-          width="45px"
-          style={{ position: "absolute", top: 0, left: 0 }}
-        />
-        <div
-          style={{
-            background: "#615DFA",
-            position: "absolute",
-            width: "37px",
-            height: "38px",
-            margin: 5,
-            top: 0,
-            left: 0,
-            clipPath:
-              "polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%)",
-            transform: "rotate(90deg)",
-          }}
-        ></div>
-        <img
-          alt=""
-          src={SmallHexPng}
-          width="28px"
-          style={{
-            position: "absolute",
-            left: "53%",
-            transform: "translateY(25px)",
-          }}
-        />
-        <Typography
-          style={{
-            position: "absolute",
-            fontSize: 12,
-            color: "white",
-            left: "70%",
-            top: "62%",
-            fontWeight: 600,
-          }}
-        >
-          23
-        </Typography>
-      </div>
+      <Badge
+        variant="dot"
+        color="primary"
+        overlap="circle"
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <div style={{ position: "relative", width: 45, height: 50 }}>
+          <img
+            alt=""
+            src={HexPng}
+            width="45px"
+            style={{ position: "absolute", top: 0, left: 0 }}
+          />
+          <div
+            style={{
+              background: "#615DFA",
+              position: "absolute",
+              width: "37px",
+              height: "38px",
+              margin: 5,
+              top: 0,
+              left: 0,
+              clipPath:
+                "polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%)",
+              transform: "rotate(90deg)",
+            }}
+          ></div>
+          <img
+            alt=""
+            src={SmallHexPng}
+            width="28px"
+            style={{
+              position: "absolute",
+              left: "53%",
+              transform: "translateY(25px)",
+            }}
+          />
+          <Typography
+            style={{
+              position: "absolute",
+              fontSize: 12,
+              color: "white",
+              left: "70%",
+              top: "62%",
+              fontWeight: 600,
+            }}
+          >
+            23
+          </Typography>
+        </div>
+      </Badge>
     </div>
   );
 };
